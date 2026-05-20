54d7e39bcefe6a46ff02c72c4eff816a34208da9
ilyasCan <ilyaskuleli61@gmail.com>
2026-05-21 00:31:34 +0300

    cors fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,8 +4,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(
+    basePath: dirname(__DIR__)
+)
+
     ->withRouting(
+
         web: __DIR__ . '/../routes/web.php',
 
         api: __DIR__ . '/../routes/api.php',
@@ -16,6 +20,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
 
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->append(
+            \Illuminate\Http\Middleware\HandleCors::class
+        );
 
         $middleware->alias([
 
@@ -28,7 +36,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
 
         //
-
+    
     })
 
     ->create();
